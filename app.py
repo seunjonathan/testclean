184dcd74230fa2b394fdf3fd04d1335f2ebe153f
Seun Jonathanm <seund@yahoo.com>
2022-10-07 14:02:21 -0600

    added layout

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
-from flask import Flask, render_template
-#, request, flash, redirect, url_for, session, logging
+from flask import Flask, render_template, request, flash, redirect, url_for, session, logging
 
 app = Flask(__name__)
 

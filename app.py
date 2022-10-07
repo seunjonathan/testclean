55e413ee05ba7fb97bee06d68e4d347f2e5698a4
Seun Jonathanm <seund@yahoo.com>
2022-10-07 13:43:51 -0600

    first

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+#, request, flash, redirect, url_for, session, logging
+
+app = Flask(__name__)
+
+
+#Home
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    return render_template('index.html')
+
+
+
+
+
+if __name__ == '__main__':    #saying if this flie was selected to run, execute whats below, but if it was ref as a library, dont run
+    app.secret_key='secret123'
+    app.run(debug=True)

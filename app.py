66af129cd1163ea45f64ef56c651f573724f35d5
seunjonathan <101889200+seunjonathan@users.noreply.github.com>
2022-10-10 16:04:02 -0600

    Update app.py

--- app.py
+++ app.py
@@ -7,14 +7,13 @@ import pymysql
 app = Flask(__name__)
 
 # Variables to Connect to the database
-conf = (
-    "host": "containers-us-west-96.railway.app",
-    "port": 6155,
-    "user": "root",
-    "password": "f7G8FVIUxKxjIaiOoY6W",
-    "charset": "utf8mb4",
-    "cursorclass": pymysql.cursors.DictCursor,
-    "database": "railway")
+conf = (host= "containers-us-west-96.railway.app",
+    port= 6155,
+    user= "root",
+    password= "f7G8FVIUxKxjIaiOoY6W",
+    charset= "utf8mb4",
+    cursorclass= pymysql.cursors.DictCursor,
+    database= "railway")
 
 
 #Home

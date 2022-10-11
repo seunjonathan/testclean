bc2f14a0fd42f330acad0ea8dd299d5d31446dc5
seunjonathan <101889200+seunjonathan@users.noreply.github.com>
2022-10-11 00:53:03 -0600

    Update app.py

--- app.py
+++ app.py
@@ -44,15 +44,15 @@ def index():
     return render_template('index.html')
 
 #Registration Form class
-class RegisterForm(Form):
-    name = StringField('Name', [validators.Length(min=1, max=50)])
-    username  = StringField('Username', [validators.Length(min=4, max=25)])
-    email  = StringField('Email', [validators.Length(min=6, max=50)])
-    password  = PasswordField('Password', [
-        validators.DataRequired(),
-        validators.EqualTo('confirm', message='Passwords do not match')
-    ])
-    confirm = PasswordField('Confirm password')
+#class RegisterForm(Form):
+#    name = StringField('Name', [validators.Length(min=1, max=50)])
+#    username  = StringField('Username', [validators.Length(min=4, max=25)])
+#    email  = StringField('Email', [validators.Length(min=6, max=50)])
+#    password  = PasswordField('Password', [
+#        validators.DataRequired(),
+#        validators.EqualTo('confirm', message='Passwords do not match')
+#    ])
+#    confirm = PasswordField('Confirm password')
 
 #@app.route('/register', methods = ['GET', 'POST'])
 #def register():
